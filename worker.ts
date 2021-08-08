629fb48e2a2f5560c561d121c134a0e83b66492e
mfbx9da4 <dalberto.adler@gmail.com>
2021-08-08 23:19:54 +0100

    fix queue

--- worker.ts
+++ worker.ts
@@ -1,38 +1,56 @@
 /// <reference lib="webworker" />
 
 import { solve } from './solver/solver'
-let shouldContinue = true
-let i = 0
-let strings = new Set<string>()
+let abortController = new AbortController()
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
-addEventListener('message', async (event) => {
-  if (event.data.type === 'start' || event.data.type === 'stop') {
-    shouldContinue = false
-    // TODO: this is flakey, race condition, is it not a queue?
-    // wait for the the ack that the iterator has stopped
-    await sleep(0)
-  }
-  if (event.data.type === 'start') {
-    strings = new Set<string>()
-    i = 0
-    shouldContinue = true
-    const input = event.data.input as Parameters<typeof solve>
-    for (const x of solve(...input)) {
-      const asStr = x.expression.join(' ')
-      if (!strings.has(asStr)) {
-        // we can get duplicates if the list has dupes
-        strings.add(asStr)
-        postMessage({ type: 'result', ...x, permutations: i })
-      }
-      if (++i % 20000 === 0) {
-        await sleep(0)
-        if (!shouldContinue) {
-          break
-        }
+const start = async (input: Parameters<typeof solve>) => {
+  abortController = new AbortController()
+  let i = 0
+  const strings = new Set<string>()
+  for (const x of solve(...input)) {
+    const asStr = x.expression.join(' ')
+    if (!strings.has(asStr)) {
+      // we can get duplicates if the list has dupes
+      strings.add(asStr)
+      postMessage({ type: 'result', ...x, permutations: i })
+    }
+    if (++i % 20000 === 0) {
+      await sleep(0)
+      if (abortController.signal.aborted) {
+        break
       }
     }
-    postMessage({ type: 'done', permutations: i })
+  }
+  postMessage({ type: 'done', permutations: i })
+}
+
+const createQueue = () => {
+  let tasks = []
+  let running = false
+  const run = async () => {
+    running = true
+    while (tasks.length) {
+      const fn = tasks.pop()
+      await fn()
+    }
+    running = false
+  }
+  const push = (fn) => {
+    tasks[0] = fn
+    if (!running) run()
+  }
+  return { push }
+}
+
+const queue = createQueue()
+
+addEventListener('message', async (event) => {
+  if (event.data.type === 'start' || event.data.type === 'stop') {
+    abortController.abort()
+  }
+  if (event.data.type === 'start') {
+    queue.push(() => start(event.data.input))
   }
 })
